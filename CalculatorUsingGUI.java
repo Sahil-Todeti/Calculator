1bac93e278f9cbf3279d40e63b2711c9a2103a9f
Sahil-Todeti <156397730+Sahil-Todeti@users.noreply.github.com>
2024-10-06 19:54:53 -0400

    Add files via upload
    Calculator using GUI

---
+++ CalculatorUsingGUI.java
@@ -0,0 +1,18 @@
+/***
+ * Creating a calculator using GUI
+ * An arithmetic operations based calculator which takes in the inputs (num1 && num2)
+ * and returns the result. And asks the user whether he/she wants to input new values into
+ * the calculator and displays the new result.
+ ***/
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+
+public class CalculatorUsingGUI {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setBackground(Color.BLACK);
+        frame.setSize(700, 600);
+        frame.setVisible(true);
+    }
+}
